7944a53802306ad6cc74422f7f9ac328c40ca4dd
Samantha Marques <sammy.marques@outlook.com>
2019-02-02 10:30:23 -0200

    Usa o Glider e funciona

--- assets/js/ko-models.js
+++ assets/js/ko-models.js
@@ -2,12 +2,27 @@ $(document).ready(function() {
     var urlPromos = "https://desafio-oi.herokuapp.com/";
     var cidades = ["rj", "sp"];
 
-    window.addEventListener('load', function(){
-        document.querySelector('.glider').Glider({
-          slidesToShow: 4,
-          slidesToScroll: auto
-        });
-    });
+    // window.addEventListener('load', function(){
+    //     document.querySelector('.glider').Glider({
+    //       slidesToShow: 4,
+    //       slidesToScroll: auto
+    //     });
+    // });
+
+    ko.bindingHandlers.gliderCarousel = {
+        init: function() {
+            new Glider(document.querySelector(".glider"), {
+                slidesToShow: 4,
+                slidesToScroll: 3,
+                draggable: true,
+                dots: "#dots",
+                arrows: {
+                prev: ".glider-prev",
+                next: ".glider-next"
+                }
+            })
+        }
+    }
 
     fetch(urlPromos).then(function(next) {
         next.json().then(function(res) {
