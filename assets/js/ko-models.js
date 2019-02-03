9bc5c71e3a98cc3b9dcb74f2e30a36ae1544149b
Samantha Marques <sammy.marques@outlook.com>
2019-02-03 00:31:03 -0200

    Conserta track do glider

--- assets/js/ko-models.js
+++ assets/js/ko-models.js
@@ -5,21 +5,8 @@ $(document).ready(function() {
         planosSelecionados: ["Todos"]
     };
     planos = [];
-    var vm = null;
-    
-
-    var select = document.getElementById("selected-option");
-    select.addEventListener("change", function() {
-        if (select.value == "Rio de Janeiro, RJ") {
-            apiUrl = "https://desafio-oi.herokuapp.com/rj";
-        } else if (select.value == "São Paulo, SP") {
-            apiUrl = "https://desafio-oi.herokuapp.com/sp";
-        } else if (select.value == "Todos") {
-            apiUrl = "https://desafio-oi.herokuapp.com/";
-        }
-        console.log(select.value);
-        MostraPlanos();
-    });
+    vm = null;
+    glider = null;
 
     function MostraPlanos() {
         fetch(apiUrl).then(function(next) {
@@ -30,111 +17,78 @@ $(document).ready(function() {
                     el.precoReal = el["preco"].split(",")[0];
                     el.precoCentavo = el["preco"].split(",")[1];
                 });         
-                planos = res;
-                if(vm == null) {
-                    vm = {
-                        planos: ko.observable(planos),
-                        localizacoes
-                    };
+                planos = res.slice(0,8);
+                if (vm == null) {
+                    vm = { planos: ko.observable(planos), localizacoes };
                     ko.applyBindings(vm);
                 }
-                else{
+                else {
                     vm.planos(planos);
-                    vm.gliderCarousel();
                 }
-
-                console.log("atualizado");
+                glider.scrollTo(0);
             });
         });
     }
     MostraPlanos();
 
+    // #region evento de mudança do select
+    var select = document.getElementById("selected-option");
+    select.addEventListener("change", function() {
+        if (select.value == "Rio de Janeiro, RJ") {
+            apiUrl = "https://desafio-oi.herokuapp.com/rj";
+        } else if (select.value == "São Paulo, SP") {
+            apiUrl = "https://desafio-oi.herokuapp.com/sp";
+        } else if (select.value == "Todos") {
+            apiUrl = "https://desafio-oi.herokuapp.com/";
+        }
+        MostraPlanos();
+    });
+    // #endregion
 
-    
-
-    // function MostraPlanos() {
-    //     var todos = [];
-    //     var rj = [];
-    //     var sp = [];
-
-    //     fetch("https://desafio-oi.herokuapp.com/rj").then(function(next) {
-    //         next.json().then(function(res) {
-    //             res.forEach(el => {
-    //                 el.dependentePreco = el["dependente-preco"];
-    //                 el.precoReal = el["preco"].split(",")[0];
-    //                 el.precoCentavo = el["preco"].split(",")[1];
-    //             });
-    //             rj = res;
-    //         })
-    //     })
-
-    //     fetch("https://desafio-oi.herokuapp.com/sp").then(function(next) {
-    //         next.json().then(function(res) {
-    //             res.forEach(el => {
-    //                 el.dependentePreco = el["dependente-preco"];
-    //                 el.precoReal = el["preco"].split(",")[0];
-    //                 el.precoCentavo = el["preco"].split(",")[1];
-    //             });
-    //             sp = res;
-    //         })
-    //     })
-
-    //     fetch("https://desafio-oi.herokuapp.com/").then(function(next) {
-    //         next.json().then(function(res) {
-    //             res.forEach(el => {
-    //                 el.dependentePreco = el["dependente-preco"];
-    //                 el.precoReal = el["preco"].split(",")[0];
-    //                 el.precoCentavo = el["preco"].split(",")[1];
-    //             });
-    //             todos = res;
-    //         })
-    //     })
-        
-    //     ko.applyBindings(rj, sp, todos, localizacoes);
-    // }
-    // MostraPlanos();
-
-
-
+    // #region configuração do glider
     ko.bindingHandlers.gliderCarousel = {
         init: function() {
-            new Glider(document.querySelector(".glider"), {
+            glider = new Glider(document.querySelector(".glider"), {
+                addTrack: false,
                 slidesToShow: 'auto',
                 slidesToScroll: 'auto',
-                itemWidth: 270,
+                itemWidth: 257,
+                scrollLock: true,
                 draggable: true,
                 dots: ".dots",
                 arrows: {
-                prev: ".glider-prev",
-                next: ".glider-next"
+                    prev: ".glider-prev",
+                    next: ".glider-next"
                 },
                 duration: 1,
                 responsive: [
-                  {
+                {
                     breakpoint: 775,
                     settings: {
-                    slidesToShow: 'auto',
-                    slidesToScroll: 'auto',
-                    itemWidth: 270,
-                    duration: 1
+                        slidesToShow: 'auto',
+                        slidesToScroll: 'auto',
+                        itemWidth: 270,
+                        duration: 1
                     }
-                  },{
+                },{
                     breakpoint: 1024,
                     settings: {
-                    slidesToShow: 'auto',
-                    slidesToScroll: 3,
-                    itemWidth: 270,
-                    duration: 1
+                        slidesToShow: 'auto',
+                        slidesToScroll: 3,
+                        itemWidth: 270,
+                        duration: 1
                     },
-                  }
+                }
                 ]
-            })
+            });
         }
     }
+    // #endregion
 
-    document.addEventListener('glider-loaded', hideFFScrollBars); /* Esconde a barra de rolagem  */
-    document.addEventListener('glider-refresh', hideFFScrollBars);/* no Firefox, scrollbarHeight */
-    function hideFFScrollBars(e){                                 /* pode mudar no futuro.       */
+    // #region esconde barra de rolagem no firefox
+    document.addEventListener('glider-loaded', hideFFScrollBars);
+    document.addEventListener('glider-refresh', hideFFScrollBars);
+    function hideFFScrollBars(e){                                 
     var scrollbarHeight = 17;
         if(/firefox/i.test(navigator.userAgent)){
             if (window.innerWidth > 575){
@@ -142,4 +96,5 @@ $(document).ready(function() {
             }
         }
     }
+    // #endregion
 });
